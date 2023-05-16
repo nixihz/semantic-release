e34d9a7339bd34a10067b1a3149a421e5ff62804
Brian Xie <yagoodidea@gmail.com>
2023-05-16 10:07:27 +0800

    feat: git tag annotation from configfile tag message

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ async function run(context, plugins) {
     logger.warn(`Skip ${nextRelease.gitTag} tag creation in dry-run mode`);
   } else {
     // Create the tag before calling the publish plugins as some require the tag to exists
-    await tag(nextRelease.gitTag, nextRelease.gitHead, { cwd, env });
+    await tag(nextRelease.gitTag, nextRelease.gitHead, options.tagMessage, { cwd, env });
     await addNote({ channels: [nextRelease.channel] }, nextRelease.gitHead, { cwd, env });
     await push(options.repositoryUrl, { cwd, env });
     if (options.pushNotes) {
